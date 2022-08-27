176a99ce9d95b9f297e2c647a4fb15bf1b0025f1
Andrew <vishnyaishere@gmail.com>
2022-08-27 18:13:55 +0300

    exc002 done

---
+++ exc002_recursive/Program.cs
@@ -0,0 +1,17 @@
+﻿// re_cursive_ factorial nd fibo
+
+double Factorial(double x){
+    if (x == 1) return 1;
+    else return x * Factorial(x - 1);
+}
+
+Console.WriteLine(Factorial(5));
+
+double Fibo(double x){
+    if (x == 1 || x == 2) return 1;
+    else return Fibo(x - 1) + Fibo(x - 2);
+}
+
+for (int i = 1; i < 10; i++){
+    Console.WriteLine(Fibo(i));
+}
